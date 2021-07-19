cad96bc0f84760f872bf153ee1b92a57f0669622
Diego Lima <diiserras@gmail.com>
2021-07-19 20:24:43 -0300

    Seller entity class

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,13 +1,18 @@
 package aplication;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Libros");
-        System.out.println(obj);
 
+        Seller seller = new Seller(21, "Diego", "diego", new Date(), 200.00, obj);
+
+        System.out.println(seller);
     }
 }
